c026699f46d8b2fd727cd1c6c68d9ba99801089f
LiliaASt <lastsatryan2704@mail.ru>
2024-06-05 20:18:27 +0300

    PEP compliance

--- lab_1/1 part/main.py
+++ lab_1/1 part/main.py
@@ -1,6 +1,7 @@
 import json
 
-def read_text_from_file(file_name: str) ->str:
+
+def read_text_from_file(file_name: str) -> str:
     try:
         with open(file_name, 'r', encoding="utf-8") as file:
             text = file.read()
@@ -12,7 +13,8 @@ def read_text_from_file(file_name: str) ->str:
         print(f"Произошла ошибка при чтении файла '{file_name}': {e}")
         return None
 
-def read_key_from_file(file_name: str)->int:
+
+def read_key_from_file(file_name: str) -> int:
     try:
         with open(file_name, 'r', encoding="utf-8") as file:
             data = json.load(file)
@@ -22,39 +24,47 @@ def read_key_from_file(file_name: str)->int:
         print(f"Файл '{file_name}' не найден.")
         return None
     except ValueError:
-        print(f"Неверный формат ключа в файле '{file_name}'. Ключ должен быть целым числом.")
+        print(f"Неверный формат ключа в файле '{
+              file_name}'. Ключ должен быть целым числом.")
         return None
     except Exception as e:
         print(f"Произошла ошибка при чтении файла '{file_name}': {e}")
         return None
 
-def write_text_to_file(text: str, file_name: str)->None:
+
+def write_text_to_file(text: str, file_name: str) -> None:
     try:
-        with open(file_name, 'w',encoding="utf-8") as file:
+        with open(file_name, 'w', encoding="utf-8") as file:
             file.write(text)
     except Exception as e:
         print(f"Произошла ошибка при записи в файл '{file_name}': {e}")
 
 
-def encode_scytale(text:str, key: int)->str:
+def encode_scytale(text: str, key: int) -> str:
     encoded_text = [''] * key
     for i, char in enumerate(text):
         index = i % key
         encoded_text[index] += char
     return ''.join(encoded_text)
 
-def decode_scytale(text:str, key: int)->str:
+
+def decode_scytale(text: str, key: int) -> str:
     decoded_text = [''] * 86
     for i, char in enumerate(text):
-        index = i% 86
+        index = i % 86
         decoded_text[index] += char
     return ''.join(decoded_text)
 
+
 if __name__ == "__main__":
-    input_text = read_text_from_file(r"C:/Users/lasts/projects/Information-security/lab_1/1 part/original_text.txt")
-    substitution = read_key_from_file(r"C:/Users/lasts/projects/Information-security/lab_1/1 part/key.json")
+    input_text = read_text_from_file(
+        r"C:/Users/lasts/projects/Information-security/lab_1/1 part/original_text.txt")
+    substitution = read_key_from_file(
+        r"C:/Users/lasts/projects/Information-security/lab_1/1 part/key.json")
     if input_text is not None and substitution is not None:
         encrypted_text = encode_scytale(input_text, substitution)
-        write_text_to_file(encrypted_text, r"C:/Users/lasts/projects/Information-security/lab_1/1 part/encrypted.txt")
+        write_text_to_file(
+            encrypted_text, r"C:/Users/lasts/projects/Information-security/lab_1/1 part/encrypted.txt")
         decoded_text = decode_scytale(encrypted_text, substitution)
-        write_text_to_file(decoded_text, r"C:/Users/lasts/projects/Information-security/lab_1/1 part/decoded.txt")
+        write_text_to_file(
+            decoded_text, r"C:/Users/lasts/projects/Information-security/lab_1/1 part/decoded.txt")
--- lab_1/2 part/main.py
+++ lab_1/2 part/main.py
@@ -1,7 +1,8 @@
 import json
 import collections
 
-def read_text_from_file(file_name: str) ->str:
+
+def read_text_from_file(file_name: str) -> str:
     try:
         with open(file_name, 'r', encoding="utf-8") as file:
             text = file.read()
@@ -13,7 +14,8 @@ def read_text_from_file(file_name: str) ->str:
         print(f"Произошла ошибка при чтении файла '{file_name}': {e}")
         return None
 
-def read_key_from_file(file_name: str)->dict:
+
+def read_key_from_file(file_name: str) -> dict:
     try:
         with open(file_name, 'r', encoding="utf-8") as file:
             data = json.load(file)
@@ -23,20 +25,23 @@ def read_key_from_file(file_name: str)->dict:
         print(f"Файл '{file_name}' не найден.")
         return None
     except ValueError:
-        print(f"Неверный формат ключа в файле '{file_name}'. Ключ должен быть целым числом.")
+        print(f"Неверный формат ключа в файле '{
+              file_name}'. Ключ должен быть целым числом.")
         return None
     except Exception as e:
         print(f"Произошла ошибка при чтении файла '{file_name}': {e}")
         return None
 
-def write_text_to_file(text: str, file_name: str)->None:
+
+def write_text_to_file(text: str, file_name: str) -> None:
     try:
-        with open(file_name, 'w',encoding="utf-8") as file:
+        with open(file_name, 'w', encoding="utf-8") as file:
             file.write(text)
     except Exception as e:
         print(f"Произошла ошибка при записи в файл '{file_name}': {e}")
 
-def write_key_to_file(data: dict, file_name:str)->None:
+
+def write_key_to_file(data: dict, file_name: str) -> None:
     try:
         with open(file_name, 'w', encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
@@ -44,30 +49,41 @@ def write_key_to_file(data: dict, file_name:str)->None:
     except Exception as e:
         print(f"Произошла ошибка при записи ключа в файл '{file_name}': {e}")
 
+
 def calculate_frequency(file_name: str) -> None:
     frequency = {}
     data = read_text_from_file(file_name).lower().replace('\n', '')
 
     frequency = collections.Counter(data)
     total = len(data)
-    normalized_frequency = {char: count/total for char, count in frequency.items()}
-    sorted_frequency = dict(sorted(normalized_frequency .items(), key=lambda x: x[1], reverse=True))
+    normalized_frequency = {char: count /
+                            total for char, count in frequency.items()}
+    sorted_frequency = dict(
+        sorted(normalized_frequency .items(), key=lambda x: x[1], reverse=True))
     return sorted_frequency
+
 
 def generate_decryption_key(encrypted_frequency: str, rus_frequency: str) -> dict:
     decryption_mapping = dict(zip(encrypted_frequency, rus_frequency))
     return decryption_mapping
 
-def decode_text(text:str, key:dict)->str:
+
+def decode_text(text: str, key: dict) -> str:
     decoded_text = ''.join(str(key.get(char, char)) for char in text)
     return decoded_text
 
 
 if __name__ == "__main__":
-    encrypted_text = read_text_from_file(r'C:/Users/lasts/projects/Information-security/lab_1/2 part/code4.txt')
-    encrypted_frequency = calculate_frequency(r'C:/Users/lasts/projects/Information-security/lab_1/2 part/code4.txt')
-    write_key_to_file(encrypted_frequency, r'C:/Users/lasts/projects/Information-security/lab_1/2 part/tex_frequencies.json')
-    rus_frequency = read_key_from_file(r'C:/Users/lasts/projects/Information-security/lab_1/2 part/rus_frequencies.json')
+    encrypted_text = read_text_from_file(
+        r'C:/Users/lasts/projects/Information-security/lab_1/2 part/code4.txt')
+    encrypted_frequency = calculate_frequency(
+        r'C:/Users/lasts/projects/Information-security/lab_1/2 part/code4.txt')
+    write_key_to_file(encrypted_frequency,
+                      r'C:/Users/lasts/projects/Information-security/lab_1/2 part/tex_frequencies.json')
+    rus_frequency = read_key_from_file(
+        r'C:/Users/lasts/projects/Information-security/lab_1/2 part/rus_frequencies.json')
     key = generate_decryption_key(encrypted_frequency, rus_frequency)
-    decod = decode_text(encrypted_text, read_key_from_file(r'C:/Users/lasts/projects/Information-security/lab_1/2 part/key.json'))
-    write_text_to_file(decod, r'C:/Users/lasts/projects/Information-security/lab_1/2 part/decoded.txt')
+    decod = decode_text(encrypted_text, read_key_from_file(
+        r'C:/Users/lasts/projects/Information-security/lab_1/2 part/key.json'))
+    write_text_to_file(
+        decod, r'C:/Users/lasts/projects/Information-security/lab_1/2 part/decoded.txt')
