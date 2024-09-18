8a35ff9f14ccac41948708882fbd5ba6af6fed65
LiliaASt <lastsatryan2704@mail.ru>
2024-09-18 03:17:41 +0300

    Test for the longest sequence of units in a block

--- lab_2/main.py
+++ lab_2/main.py
@@ -42,6 +42,42 @@ def same_bits_test (number: str) -> float:
         print(f"Произошла ошибка при чтении '{number}': {e}")
         return None
 
+def long_sequence_test (number: str) -> float:
+    try:
+        size = 8
+        pi = {0: 0.2148, 1: 0.3672, 2: 0.2305, 3: 0.1875}
+        v = {0: 0, 1: 0, 2: 0, 3: 0}
+        blocks = [number[i:i+size] for i in range(0, len(number), size)]
+
+        for block in blocks:
+            current = 0
+            current_max = 0
+            for i in block:
+                if i == '1':
+                    current += 1
+                    current_max = max(current_max, current)
+                else:
+                    current = 0
+
+            if current_max <= 1:
+                v[0] += 1
+            elif current_max == 2:
+                v[1] += 1
+            elif current_max == 3:
+                v[2] += 1
+            elif current_max >= 4:
+                v[3] += 1
+
+            X = 0
+            for i in range(4):
+                X += pow((v[i]-16*pi[i]), 2)/(16*pi[i])
+            value = gammainc(1.5, (X/2))
+        return value
+
+    except Exception as e:
+        print(f"Произошла ошибка при чтении '{number}': {e}")
+        return None
+
 if __name__ == "__main__":
     try:
         with open(gen_sequence, 'r', encoding="utf-8") as file:
@@ -52,12 +88,14 @@ if __name__ == "__main__":
         cpp_results = {
             "frequency_test": frequency_test(cpp_sequence),
             "same_bits_test": same_bits_test(cpp_sequence),
+            "long_sequence_test ": long_sequence_test(cpp_sequence)
         }
         print (cpp_results)
 
         java_results = {
             "frequency_test": frequency_test(java_sequence),
             "same_bits_test": same_bits_test(java_sequence),
+            "long_sequence_test ": long_sequence_test(java_sequence)
         }
         print (java_results)
 
