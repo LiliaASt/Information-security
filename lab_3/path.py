85c30bf405055695e69319ac7a6ba5edeb994e5d
LiliaASt <lastsatryan2704@mail.ru>
2024-09-30 00:52:52 +0300

    writing a console application and specifying paths

---
+++ lab_3/path.py
@@ -0,0 +1,9 @@
+symmetric_key_path = "C:/Users/lasts/projects/Information-security/lab_3/key/symmetric_key.txt"
+public_key_path = "C:/Users/lasts/projects/Information-security/lab_3/key/public_key.pem"
+private_key_path =  "C:/Users/lasts/projects/Information-security/lab_3/key/private_key.pem"
+encrypted_symmetric_key_path = "C:/Users/lasts/projects/Information-security/lab_3/key/encrypted_symmetric_key.txt"
+decrypted_symmetric_key_path = "C:/Users/lasts/projects/Information-security/lab_3/key/decrypted_symmetric_key.txt"
+
+decrypted_text = "C:/Users/lasts/projects/Information-security/lab_3/text/decrypted_text.txt"
+encrypted_text = "C:/Users/lasts/projects/Information-security/lab_3/text/encrypted_text.txt"
+text = "C:/Users/lasts/projects/Information-security/lab_3/text/text.txt"
